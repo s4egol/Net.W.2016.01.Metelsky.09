66b448ffc3d310c72bf29d5189dafd808c317350
Vanya <vanya.metelsky@gmail.com>
2016-11-10 12:30:51 +0300

    Done

---
+++ Task2/SortMatrix.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public class SortMatrix
+    {
+        /// <summary>
+        /// Sort a matrix of integers
+        /// </summary>
+        /// <param name="matrix">Sortable matrix</param>
+        /// <param name="comparer"></param>
+        public static void Sorting(int[][] matrix, IComparer<int[]> comparer)
+        {
+            if (matrix == null || comparer == null)
+                throw new ArgumentNullException();
+
+            for (int i = 0; i < matrix.Length - 1; i++)
+            {
+                for (int j = i + 1; j < matrix.Length; j++)
+                {
+                    if (comparer.Compare(matrix[i], matrix[j]) > 0)
+                    {
+                        Swap(ref matrix[i], ref matrix[j]);
+                    }
+                }
+            }
+        }
+
+        public static void Sorting(int[][] matrix, Func<int[], int[], int> comparer)
+        {
+            if (comparer == null)
+                throw new ArgumentNullException();
+
+            Sorting(matrix, new DelegateWithSupportInterface<int[]>(comparer));
+        }
+
+
+        private class DelegateWithSupportInterface<T> : IComparer<T>
+        {
+            private readonly Func<T, T, int> comparerDelegate;
+
+            public DelegateWithSupportInterface(Func<T, T, int> comparer)
+            {
+                comparerDelegate = comparer;
+            }
+
+            public int Compare(T x, T y)
+            {
+                return comparerDelegate(x, y);
+            }
+
+        }
+
+
+        /// <summary>
+        /// Swaps the elements of the array
+        /// </summary>
+        /// <param name="string1">First string of array</param>
+        /// <param name="string2">Second string of array</param>
+		private static void Swap(ref int[] string1, ref int[] string2)
+        {
+            int[] temp = string1;
+            string1 = string2;
+            string2 = temp;
+        }
+    }
+}
---
+++ Task2/SortMatrixCopy.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class SortMatrixCopy
+    {
+        public static void Sorting(int[][] matrix, IComparer<int[]> comparer)
+        {
+            if (comparer == null)
+                throw new ArgumentNullException();
+
+            Sorting(matrix, comparer.Compare);
+        }
+
+        public static void Sorting(int[][] matrix, Func<int[], int[], int> comparerDelegate)
+        {
+            if (matrix == null || comparerDelegate == null)
+                throw new ArgumentNullException();
+
+            for (int i = 0; i < matrix.Length - 1; i++)
+            {
+                for (int j = i + 1; j < matrix.Length; j++)
+                {
+                    if (comparerDelegate(matrix[i], matrix[j]) > 0)
+                    {
+                        Swap(ref matrix[i], ref matrix[j]);
+                    }
+                }
+            }
+        }
+
+        private static void Swap(ref int[] string1, ref int[] string2)
+        {
+            int[] temp = string1;
+            string1 = string2;
+            string2 = temp;
+        }
+    }
+}
